52d1a6be2a5be1f1c846043ffe4a9e1ba0d7705a
Exodus Buyser <ebuyser1@studentmail.nait.ca>
2016-11-07 16:05:00 -0700

    ReverveEnginnerETools

---
+++ eToolsWebSite/App_Code/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(eToolsWebSite.Startup))]
+namespace eToolsWebSite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
